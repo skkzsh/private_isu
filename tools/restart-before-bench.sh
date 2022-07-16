698765e0d317558a53119acfa4c8ce3d103d9870
skkzsh <skkzsh12@gmail.com>
2022-07-17 02:42:18 +0900

    Fix script

--- tools/restart-before-bench.sh
+++ tools/restart-before-bench.sh
@@ -3,7 +3,7 @@ set -euvx
 
 sudo truncate -s 0 -c /var/log/nginx/access.log
 sudo truncate -s 0 -c /var/log/nginx/error.log
-sudo truncate -s 0 -c /var/log/mysql/slow.log
+sudo truncate -s 0 -c /var/log/mysql/mysql-slow.log
 sudo truncate -s 0 -c /var/log/mysql/error.log
 # mysqladmin flush-logs
 
