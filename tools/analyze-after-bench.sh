cf3e3569f2975bff4ffd426b0c3485bab82da902
skkzsh <skkzsh12@gmail.com>
2022-07-16 23:08:00 +0900

    Update scripts

--- tools/analyze-after-bench.sh
+++ tools/analyze-after-bench.sh
@@ -6,7 +6,7 @@ sudo cat /var/log/nginx/access.log |
 	tee ~/logs/alp-$(date +%Y%m%d-%H%M%S).log
 
 # sudo mysqldumpslow /var/log/mysql/mysql-slow.log | tee ~/logs/slow-$(date +%Y%m%d-%H%M%S).log
-# sudo pt-query-digest /var/log/mysql/mysql-slow.log | tee ~/logs/pt-query-digest-$(date +%Y%m%d-%H%M%S).log
+sudo pt-query-digest /var/log/mysql/mysql-slow.log | tee ~/logs/pt-query-digest-$(date +%Y%m%d-%H%M%S).log
 
 # go tool pprof -http=:10060 http://localhost:6060/debug/pprof/profile
 
--- tools/restart-before-bench.sh
+++ tools/restart-before-bench.sh
@@ -2,9 +2,9 @@
 set -euvx
 
 sudo truncate -s 0 -c /var/log/nginx/access.log
-# sudo truncate -s 0 -c /var/log/nginx/error.log
+sudo truncate -s 0 -c /var/log/nginx/error.log
 sudo truncate -s 0 -c /var/log/mysql/slow.log
-# sudo truncate -s 0 -c /var/log/mysql/error.log
+sudo truncate -s 0 -c /var/log/mysql/error.log
 # mysqladmin flush-logs
 
 cd /home/isucon/private_isu/webapp/golang
