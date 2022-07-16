6d1480528acefd2138540eb3bfa3ee69d6eaf02d
skkzsh <skkzsh12@gmail.com>
2022-07-16 18:14:01 +0900

    Create script and modify nginx.conf

---
+++ tools/restart-before-bench.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euvx
+
+sudo truncate -s 0 -c /var/log/nginx/access.log
+# sudo truncate -s 0 -c /var/log/nginx/error.log
+sudo truncate -s 0 -c /var/log/mysql/slow.log
+# sudo truncate -s 0 -c /var/log/mysql/error.log
+# mysqladmin flush-logs
+
+cd /home/isucon/private_isu/webapp/golang
+make
+sudo systemctl restart isu-go
+
+sudo systemctl restart mysql
+sudo systemctl restart nginx
+
