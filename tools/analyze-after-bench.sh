fcb4a97d9e343be277f036f0878ae86598735f20
skkzsh <skkzsh12@gmail.com>
2022-07-16 22:43:17 +0900

    Update scripts

--- tools/analyze-after-bench.sh
+++ tools/analyze-after-bench.sh
@@ -3,10 +3,10 @@ set -euvx
 
 sudo cat /var/log/nginx/access.log |
 	alp json -q  --qs-ignore-values --sort avg -r -m '^/posts/[0-9]*.$','^/@[a-z]*$','^/image/[0-9]*.jpg$','^/image/[0-9]*.png$','^/image/[0-9]*.gif$' |
-	tee ~/log/alp-$(date +%Y%m%d-%H%M%S).log
+	tee ~/logs/alp-$(date +%Y%m%d-%H%M%S).log
 
-# sudo mysqldumpslow /var/log/mysql/mysql-slow.log | tee ~/log/slow-$(date +%Y%m%d-%H%M%S).log
-# sudo pt-query-digest /var/log/mysql/mysql-slow.log | tee ~/log/pt-query-digest-$(date +%Y%m%d-%H%M%S).log
+# sudo mysqldumpslow /var/log/mysql/mysql-slow.log | tee ~/logs/slow-$(date +%Y%m%d-%H%M%S).log
+# sudo pt-query-digest /var/log/mysql/mysql-slow.log | tee ~/logs/pt-query-digest-$(date +%Y%m%d-%H%M%S).log
 
 # go tool pprof -http=:10060 http://localhost:6060/debug/pprof/profile
 
