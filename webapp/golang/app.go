c8a737216b37f7d0d8d70edb3f32937e8eb51fbe
skkzsh <skkzsh12@gmail.com>
2022-07-21 02:25:02 +0900

    what to hack

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -183,12 +183,12 @@ func makePosts(results []Post, csrfToken string, allComments bool) ([]Post, erro
 	var posts []Post
 
 	for _, p := range results {
-		err := db.Get(&p.CommentCount, "SELECT COUNT(*) AS `count` FROM `comments` WHERE `post_id` = ?", p.ID)
+		err := db.Get(&p.CommentCount, "SELECT COUNT(*) AS `count` FROM `comments` WHERE `post_id` = ?", p.ID) // FIXME: 呼出多
 		if err != nil {
 			return nil, err
 		}
 
-		query := "SELECT * FROM `comments` WHERE `post_id` = ? ORDER BY `created_at` DESC"
+		query := "SELECT * FROM `comments` WHERE `post_id` = ? ORDER BY `created_at` DESC" // FIXME: 呼出多，
 		if !allComments {
 			query += " LIMIT 3"
 		}
@@ -418,7 +418,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) { // FIXME: slow
 
 	results := []Post{}
 
-	// err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC") // FIXME: slow
+	// err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
 	err := db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 ORDER BY p.created_at DESC LIMIT 20") // FIXME: postsPerPage
 	if err != nil {
 		log.Print(err)
@@ -466,7 +466,7 @@ func getAccountName(w http.ResponseWriter, r *http.Request) { // FIXME: slow
 	results := []Post{}
 
 	// err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC", user.ID)
-	err = db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 AND p.user_id = ? ORDER BY p.created_at DESC LIMIT 20", user.ID) // FIXME: postsPerPage
+	err = db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 AND p.user_id = ? ORDER BY p.created_at DESC LIMIT 20", user.ID) // FIXME: postsPerPage, slow
 	if err != nil {
 		log.Print(err)
 		return
@@ -479,7 +479,7 @@ func getAccountName(w http.ResponseWriter, r *http.Request) { // FIXME: slow
 	}
 
 	commentCount := 0
-	err = db.Get(&commentCount, "SELECT COUNT(*) AS count FROM `comments` WHERE `user_id` = ?", user.ID)
+	err = db.Get(&commentCount, "SELECT COUNT(*) AS count FROM `comments` WHERE `user_id` = ?", user.ID) // FIXME: slow
 	if err != nil {
 		log.Print(err)
 		return
