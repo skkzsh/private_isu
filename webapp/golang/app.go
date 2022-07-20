3a3ff6b65707f234f55ea8e700d2e45dda760e07
skkzsh <skkzsh12@gmail.com>
2022-07-21 01:21:52 +0900

    use join against slow query

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -417,7 +417,8 @@ func getIndex(w http.ResponseWriter, r *http.Request) { // FIXME: slow
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC") // FIXME: slow
+	// err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC") // FIXME: slow
+	err := db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 ORDER BY p.created_at DESC LIMIT 20") // FIXME: postsPerPage
 	if err != nil {
 		log.Print(err)
 		return
