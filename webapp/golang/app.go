39eb5c74fd404e371b424bc1912f157499e00eb0
skkzsh <skkzsh12@gmail.com>
2022-07-21 01:37:44 +0900

    横展開

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -235,19 +235,20 @@ func makePosts(results []Post, csrfToken string, allComments bool) ([]Post, erro
 
 		p.Comments = comments
 
-		err = db.Get(&p.User, "SELECT * FROM `users` WHERE `id` = ?", p.UserID) // FIXME: 呼出多
-		if err != nil {
-			return nil, err
-		}
+		// err = db.Get(&p.User, "SELECT * FROM `users` WHERE `id` = ?", p.UserID) // FIXME: 呼出多
+		// if err != nil {
+		// 	return nil, err
+		// }
 
 		p.CSRFToken = csrfToken
 
-		if p.User.DelFlg == 0 {
-			posts = append(posts, p)
-		}
-		if len(posts) >= postsPerPage {
-			break
-		}
+		// if p.User.DelFlg == 0 {
+		// 	posts = append(posts, p)
+		// }
+		posts = append(posts, p)
+		// if len(posts) >= postsPerPage {
+		// 	break
+		// }
 	}
 
 	return posts, nil
@@ -464,7 +465,8 @@ func getAccountName(w http.ResponseWriter, r *http.Request) { // FIXME: slow
 
 	results := []Post{}
 
-	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC", user.ID)
+	// err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC", user.ID)
+	err = db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 AND p.user_id = ? ORDER BY p.created_at DESC LIMIT 20", user.ID) // FIXME: postsPerPage
 	if err != nil {
 		log.Print(err)
 		return
@@ -552,7 +554,8 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC", t.Format(ISO8601Format))
+	// err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC", t.Format(ISO8601Format))
+	err = db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 AND p.created_at <= ? ORDER BY p.created_at DESC LIMIT 20", t.Format(ISO8601Format)) // FIXME: postsPerPage
 	if err != nil {
 		log.Print(err)
 		return
@@ -588,7 +591,8 @@ func getPostsID(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT * FROM `posts` WHERE `id` = ?", pid)
+	// err = db.Select(&results, "SELECT * FROM `posts` WHERE `id` = ?", pid)
+	err = db.Select(&results, "SELECT p.* FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 AND p.id = ?", pid)
 	if err != nil {
 		log.Print(err)
 		return
