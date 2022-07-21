9bd8d95e06068ae98852523145150be2dcd5a9de
skkzsh <skkzsh12@gmail.com>
2022-07-21 21:33:21 +0900

    Use STRAIGHT_JOIN more

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -442,7 +442,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) { // FIXME: slow
 	results := []Post{}
 
 	// err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
-	err := db.Select(&results, "SELECT p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 ORDER BY p.created_at DESC LIMIT 20") // FIXME: postsPerPage
+	err := db.Select(&results, "SELECT STRAIGHT_JOIN p.id, p.user_id, p.body, p.mime, p.created_at FROM `posts` p JOIN `users` u ON p.user_id = u.id WHERE u.del_flg = 0 ORDER BY p.created_at DESC LIMIT 20") // FIXME: postsPerPage
 	if err != nil {
 		log.Print(err)
 		return
